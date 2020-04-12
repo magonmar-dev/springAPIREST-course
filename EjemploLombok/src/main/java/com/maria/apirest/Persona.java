022262c7660b365cf66d896834fb93e848d3dfdb
mariagm173 <mariagm173@hotmail.com>
2020-04-12 19:12:46 +0200

    EjemploLombok 001 - Uso de la librería Lombok

---
+++ EjemploLombok/src/main/java/com/maria/apirest/Persona.java
@@ -0,0 +1,13 @@
+package com.maria.apirest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Persona {
+
+	private String dni;
+	private String nombre;
+	private String apellidos;
+}
---
+++ EjemploLombok/src/test/java/com/maria/apirest/EjemploApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maria.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
